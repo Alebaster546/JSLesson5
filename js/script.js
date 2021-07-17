bf36a3eedc750269d1af52d2fa77c9299f13edc9
Alebaster546 <_vurtz88_@mail.ru>
2021-07-17 11:27:45 +0300

    lesson5

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const lengthnode = document.body.childNodes.length;
+const lengthteg = document.body.children.length;
+
+// список всех узлов body
+console.log("Кол-во узлов body =" + lengthnode);
+// ссписок дочернихъ узлов являющимися тегами
+console.log("Кол-во узлов тегов body =" + lengthteg);
